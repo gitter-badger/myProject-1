8fec680c118d83e94e231ee93d41200e957730c2
salim.belim <salim.belim@moneysupermarket.com>
2016-05-13 17:24:57 +0100

    removing unused LOG

--- src/main/java/com/sainsburys/productscrapper/reporter/ConsoleReporter.java
+++ src/main/java/com/sainsburys/productscrapper/reporter/ConsoleReporter.java
@@ -2,12 +2,9 @@ package com.sainsburys.productscrapper.reporter;
 
 import com.sainsburys.productscrapper.model.Result;
 import com.sainsburys.productscrapper.serializer.ResultSerializer;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ConsoleReporter implements Reporter {
-
-    private Logger LOG = Logger.getLogger(ConsoleReporter.class);
 
     @Autowired
     private ResultSerializer resultSerializer;
